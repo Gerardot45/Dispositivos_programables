97d92d71f141e2ba3cca640a815713a47faddb10
Gerardo <kalicech@gmail.com>
2021-01-15 22:41:29 -0600

    Se cambiaron los nombres y se agregó la práctica del nombre

---
+++ Practica4_GERARDO.X/build/default/production/gerardo.o.d
@@ -0,0 +1,3 @@
+build/default/production/gerardo.o:  \
+	C:/Users/gerrr/MPLABXProjects/Practica4_GERARDO.X/gerardo.asm  \
+
---
+++ Practica4_GERARDO.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Practica4_GERARDO.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Practica4_GERARDO.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=practica4gerardo.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/practica4gerardo.x.tar
